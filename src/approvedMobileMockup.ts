2ea730c0656bff16e6a0b522ff236fa6584a79c5
chistocrym-wq <chistocrym@gmail.com>
2026-09-13 00:55:25 +0300

    Assemble approved mobile artwork

---
+++ src/approvedMobileMockup.ts
@@ -0,0 +1,10 @@
+import p1 from './approved-mobile/p1';
+import p2 from './approved-mobile/p2';
+import p3 from './approved-mobile/p3';
+import p4 from './approved-mobile/p4';
+import p5 from './approved-mobile/p5';
+import p6 from './approved-mobile/p6';
+import p7 from './approved-mobile/p7';
+import p8 from './approved-mobile/p8';
+
+export const APPROVED_MOBILE_MOCKUP = `data:image/webp;base64,${p1}${p2}${p3}${p4}${p5}${p6}${p7}${p8}`;
