de6f608b61d11506138d6fdc2c95e0708af570f8
chistocrym-wq <chistocrym@gmail.com>
2026-09-13 15:03:29 +0300

    Load premium Otto voice control styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './ottoPremiumSystem.css';
+import './ottoPremiumVoice.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
