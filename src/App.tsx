438a274a5511c9aa74592dcb21a8c500d6590100
chistocrym-wq <chistocrym@gmail.com>
2026-09-13 20:48:45 +0300

    Apply premium visual system as final app style layer

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import './ottoFinalPolish.css';
 import './ottoHomeReact.css';
 import './ottoUnifiedPages.css';
 import './ottoUnifiedPagesFix.css';
+import './ottoVisualSystem.css';
 import '@/data/lesen/registerExtraSets';
 
 const Instructions = lazy(() => import('@/components/Instructions').then((m) => ({ default: m.Instructions })));
