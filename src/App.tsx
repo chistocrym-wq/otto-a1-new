e774302e0935a020a6c1c12b0cb78e9d6e7693e3
chistocrym-wq <chistocrym@gmail.com>
2026-09-13 21:10:00 +0300

    Apply user-requested visual fixes last

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import './ottoUnifiedPages.css';
 import './ottoUnifiedPagesFix.css';
 import './ottoVisualSystem.css';
 import './ottoVisualPages.css';
+import './ottoUserFixes.css';
 import '@/data/lesen/registerExtraSets';
 
 const Instructions = lazy(() => import('@/components/Instructions').then((m) => ({ default: m.Instructions })));
