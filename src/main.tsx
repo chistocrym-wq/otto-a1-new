4e3f3c0f21b9095060d9d4f1cc706f3d5c973145
chistocrym-wq <chistocrym@gmail.com>
2026-09-15 00:41:48 +0300

    Initialize PWA install support

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,11 @@ import './index.css';
 import './ottoPremiumSystem.css';
 import './ottoPremiumVoice.css';
 import { installGermanSpeechPolicy } from './lib/germanSpeechPolicy';
+import { initPwaInstall } from './lib/pwaInstall';
 
 installGermanSpeechPolicy();
+initPwaInstall();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <StrictMode><App /></StrictMode>
 );
