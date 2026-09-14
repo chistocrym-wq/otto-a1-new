5921455dfcfd856ebb0f1fb33663e701a30b3555
chistocrym-wq <chistocrym@gmail.com>
2026-09-14 19:57:40 +0300

    Load targeted OTTO point fixes

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import './ottoFinalPolish.css';
 import './ottoHomeReact.css';
 import './ottoUnifiedPages.css';
 import './ottoUnifiedPagesFix.css';
+import './ottoPointFixes.css';
 import '@/data/lesen/registerExtraSets';
 
 const Instructions = lazy(() => import('@/components/Instructions').then((m) => ({ default: m.Instructions })));
